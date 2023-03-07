ad176a8d8a10dd2c5fe286629c99995013f71fa2
Gustavo Resende Soares <113553839+Naufra1@users.noreply.github.com>
2023-03-07 11:20:42 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import usuarioController from './controllers/usuario-controller'
 import tarefaController from './controllers/tarefa-controller'
 
 const app = express()
+app.use(express.json())
 
 usuarioController(app)
 tarefaController(app)
--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -3,14 +3,32 @@ import { bdUsuarios } from "../infra/db"
 function usuarioController(app) {
   app.get('/usuario', listar)
   app.post('/usuario', inserir)
+  app.get('/usuario/email/:email', verificarEmail)
+  app.delete('usuario/email/:email', deletarEmail)
+
   function listar(req, res) {
       const usuarios = bdUsuarios
-      // Devolve a lista de Usuarios
       res.send(usuarios)
   }
+  
   function inserir(req, res) {
       res.send('Método POST')
       console.log(req.body)
+  }
+
+  function verificarEmail(req, res) {
+    const usuario = bdUsuarios.find(usuario => usuario.email === req.params.email)
+
+    if(!usuario) {
+      res.status(404).send('Não encontrado')
+    }
+    res.send(usuario)
+  }
+
+  function deletarEmail(req, res) {
+    const usuario = bdUsuarios.find(usuario => usuario.email === req.params.email)
+    
+    res.send(`{${usuario} deletado}`)
   }
 }
 
